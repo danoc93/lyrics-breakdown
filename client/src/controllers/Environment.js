f85830d98cdcc8bc37436971c0a0f26b6665f16d
Daniel Ortiz Costa <deortiz93@gmail.com>
2018-03-31 02:21:43 -0400

    Update Environment.js
    Server golden update

--- client/src/controllers/Environment.js
+++ client/src/controllers/Environment.js
@@ -5,6 +5,5 @@ Controller: Environment.
 // ROOT: Use http://localhost:5000 locally.
 
 export const Environment = {
-  // BACKEND_ROOT : 'https://lyrics-breakdown-server.herokuapp.com',
-  BACKEND_ROOT : 'http://localhost:5000'
+  BACKEND_ROOT : 'https://lyrics-breakdown-server.herokuapp.com'
 };
