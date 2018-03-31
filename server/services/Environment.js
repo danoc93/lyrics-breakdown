b850eb5e5885f4f6420044794c2ff1a8b9f66872
Daniel Ortiz Costa <deortiz93@gmail.com>
2018-03-31 02:24:01 -0400

    Update Environment.js
    Fixing deploy data.

--- server/services/Environment.js
+++ server/services/Environment.js
@@ -2,11 +2,10 @@
  Controller: Environment.
  */
 
-// ROOT: Use http://localhost:5000 locally.
+// ROOT: Use mongodb://localhost:27017/lt-db locally.
 
 const Environment = {
-    MONGO_URI:'mongodb://localhost:27017/lt-db',
-    // MONGO_URI: 'mongodb://lt-sandbox:trb-b8o-vYz-nrn@ds127899.mlab.com:27899/lt-db'
+    MONGO_URI:'mongodb://lt-sandbox:trb-b8o-vYz-nrn@ds127899.mlab.com:27899/lt-db'
 };
 
 module.exports = Environment;
