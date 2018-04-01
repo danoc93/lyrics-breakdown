e34da2aaf4f8340b9f160a1c187e6fe48c61cec1
daniel <deortiz93@gmail.com>
2018-04-01 19:22:59 -0400

    Force adding golden environments

--- server/services/Environment.js
+++ server/services/Environment.js
@@ -2,10 +2,12 @@
  Controller: Environment.
  */
 
-// ROOT: Use mongodb://localhost:27017/lt-db locally.
-
+// Use mongodb://localhost:27017/lt-db locally.
+// Use mongodb://ds127899.mlab.com:27899/lt-db remotely.
 const Environment = {
-    MONGO_URI:'mongodb://localhost:27017/lt-db',
+    MONGO_URI:'mongodb://ds127899.mlab.com:27899/lt-db',
+    MONGO_USER:'lt-sandbox',
+    MONGO_PASS:'trb-b8o-vYz-nrn',
     SESSION_SECRET: 'universityoftears'
 };
 
