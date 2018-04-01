1a995dcd888ebf1e741dd5cdb49eaeb4320871f0
DerrickAfrifa <derrick.dx@hotmail.com>
2018-04-01 00:39:07 -0400

    delete reduant code

--- client/src/controllers/LeaderboardService.js
+++ client/src/controllers/LeaderboardService.js
@@ -30,30 +30,6 @@ class LeaderboardService{
       requestUtils.preparePromise(
         'POST', body, url_req, callbackSuccess, callbackError); 
     }
-
-
-
-    // var dummyList = [
-    //   {'key' : 0, 'name': 'person1', 'flag' : '🇦🇷', 'p': 1000},
-    //   {'key' : 1, 'name': 'person2', 'flag' : '🇧🇪', 'p': 800},
-    //   {'key' : 2, 'name': 'person3', 'flag' : '🇨🇦', 'p': 500},
-    //   {'key' : 3, 'name': 'person4', 'flag' : '🇩🇰', 'p': 500},
-    //   {'key' : 4, 'name': 'person5', 'flag' : '🇨🇮', 'p': 300}
-    // ];
-
-    // var dummyList2 = [
-    //   {'key' : 0, 'name': 'person3', 'flag' : '🇨🇦', 'p': 500},
-    //   {'key' : 1, 'name': 'person7', 'flag' : '🇨🇦', 'p': 200},
-    //   {'key' : 2, 'name': 'person6', 'flag' : '🇨🇦', 'p': 150}
-    // ];
-
-    // if(country)
-    // {
-    //   return dummyList2;
-    // }else {
-    //   return dummyList;
-    // }
-
   }
 
   static buildGlobalLeaderBoard(size){
@@ -132,39 +108,6 @@ class LeaderboardService{
       }, (err)=>{
         console.log(err);
       });
-
-
-
-
-
-  //   var currentLeaders = LeaderboardService.getGlobalLeaderboardSummary(country);
-
-  //   return (
-  //     <table className="table table-striped">
-  //       <thead>
-  //         <tr>
-  //           <th scope="col">#</th>
-  //           <th scope="col">Name</th>
-  //           <th scope="col">Points</th>
-  //           <th scope="col"></th>
-  //         </tr>
-  //       </thead>
-  //       <tbody>
-  //         {
-  //           currentLeaders.map(
-  //           function(item, index){
-  //             return (<tr key={item.key}>
-  //                       <th scope="row">{ index + 1 }</th>
-  //                       <td>{ item.name }</td>
-  //                       <td>{ item.p }</td>
-  //                       <td>{ item.flag }</td>
-  //                     </tr>)
-
-  //           })
-  //         }
-  //       </tbody>
-  //     </table>
-  //    );
   }
 
 }
