81b4cf3c60d1d25c6c2d0ccfd44af44027118781
daniel <deortiz93@gmail.com>
2018-03-31 22:33:51 -0400

    Adding server elements for mini games

--- server/routes/GameRouter.js
+++ server/routes/GameRouter.js
@@ -3,15 +3,19 @@ var router = express.Router();
 
 var MusicService = require('./../services/MusicService');
 
+var GAMES = {'0' : 'ca', '1' : 'us', '2' : 'jp'};
+var LIMIT_QUERY = 15;
+
 router.get('/:game_id', function(req, res, next) {
 
-  var gameIdentifier = req.params.game_id;
+  // This has to change in the future to support dynamic settings.
+  var gameIdentifier = GAMES[req.params.game_id];
+  if (gameIdentifier == null) gameIdentifier = GAMES[1];
 
   let handler = function(data){ res.send(data); };
   let musicService = new MusicService(handler);
 
-  // TO DO: interface DB to get the actual settings for a game id!
-  musicService.getTracks(15, 'ca');
+  musicService.getTracks(LIMIT_QUERY, gameIdentifier);
 
 });
 
